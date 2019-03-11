750e6ba3bc22a9e786fe414894342b4b151e3068
Robert Tyrees <robert.tyree@stinkstudios.com>
2019-03-11 12:43:46 +0000

    add colour borders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import ReactPlayer from "react-player";
 import "./App.css";
 
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
 const App = ({ location }) => {
   let [shouldPlay, updatePlayState] = useState(true);
-
+  let [borderState, updateBorderState] = useState(1);
   const vidUrl = location.search.slice(1);
+
+  useInterval(() => {
+    borderState < 4 ? updateBorderState(borderState++) : updateBorderState(1);
+  }, 1300);
+
   return (
     <div className="App">
-      <button className="Button" onClick={() => updatePlayState(!shouldPlay)}>
-        <ReactPlayer
-          className="Video"
-          url={vidUrl}
-          playing={shouldPlay}
-          loop
-          volume={0}
-          muted
-          width={360}
-        />
-      </button>
+      <div className={`Container Border-${borderState}`}>
+        <button className="Button" onClick={() => updatePlayState(!shouldPlay)}>
+          <ReactPlayer
+            url={vidUrl}
+            playing={shouldPlay}
+            loop
+            volume={0}
+            muted
+            width={360}
+          />
+        </button>
+      </div>
     </div>
   );
 };
